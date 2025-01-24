4721418ae17710edf2f0c5b5b475100fff51866d
Don <496156+nikolat@users.noreply.github.com>
2025-01-24 20:14:10 +0900

    node_modules 更新

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	// @ts-expect-error なんもわからんかも
 	plugins: [sveltekit()],
 
 	test: {

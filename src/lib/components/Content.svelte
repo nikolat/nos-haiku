11c16e0ec9fad6e9f776b6d7f72e64577c181843
Don <496156+nikolat@users.noreply.github.com>
2025-06-05 17:31:37 +0900

    node_modules 更新

--- src/lib/components/Content.svelte
+++ src/lib/components/Content.svelte
@@ -92,7 +92,7 @@
 
 	const [matchesIterator, plainTexts, emojiUrls] = $derived(getExpandTagsList(content, tags));
 
-	const nip19decode = (text: string) => {
+	const nip19decode = (text: string): nip19.DecodedResult | null => {
 		try {
 			return nip19.decode(text);
 		} catch (_error) {
